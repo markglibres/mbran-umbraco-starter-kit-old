8c4310b021c2e0160ddc7c3d69ddceaa991aa9bd
Mark Gil Libres <markglibres@gmail.com>
2017-11-17 12:58:25 +1100

    added extension for ipublishedcontent

---
+++ src/MBran.Umbraco.Core/Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Umbraco.Core.Models;
+
+namespace MBran.Umbraco.Core
+{
+    public static class PublishedContentExtensions
+    {
+        public static T As<T>(this IPublishedContent content)
+            where T : class, IPublishedContent
+        {
+            T newObject = (T)Activator.CreateInstance(typeof(T), content);
+            return newObject;
+        }
+    }
+}
