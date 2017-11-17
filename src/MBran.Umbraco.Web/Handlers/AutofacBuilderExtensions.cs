9703b4aa009ff28a6e629e09087ab8ab55c3d7ec
Mark Gil Libres <markglibres@gmail.com>
2017-11-17 12:34:27 +1100

    added Autofac

---
+++ src/MBran.Umbraco.Web/Handlers/AutofacBuilderExtensions.cs
@@ -0,0 +1,67 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using System.Linq;
+using System.Reflection;
+using System.Web.Compilation;
+using System.Web.Mvc;
+using Umbraco.Web;
+
+namespace MBran.Umbraco.Web
+{
+    public static class AutofacBuilderExtensions
+    {
+        public static ContainerBuilder RegisterUmbraco(this ContainerBuilder builder)
+        {
+            builder.RegisterCustomControllers(typeof(UmbracoApplication).Assembly);
+            builder.RegisterApiControllers(typeof(UmbracoApplication).Assembly);
+
+
+            return builder;
+        }
+
+        public static ContainerBuilder RegisterModules(this ContainerBuilder builder)
+        {
+            var assemblies = BuildManager.GetReferencedAssemblies().Cast<Assembly>();
+            foreach (var assembly in assemblies)
+            {
+                builder.RegisterAssemblyModules(assembly);
+            }
+            return builder;
+        }
+
+        public static ContainerBuilder RegisterCustomControllers(this ContainerBuilder builder, Assembly executingAssembly)
+        {
+            builder.RegisterAssemblyTypes(executingAssembly)
+                .Where(c => c.Name.EndsWith("Controller"))
+                .AsImplementedInterfaces();
+
+            return builder;
+        }
+
+        public static ContainerBuilder RegisterServices(this ContainerBuilder builder, Assembly executingAssembly)
+        {
+            builder.RegisterAssemblyTypes(executingAssembly)
+                .Where(c => c.Name.EndsWith("Service"))
+                .AsImplementedInterfaces();
+
+            return builder;
+        }
+        public static ContainerBuilder RegisterRepositories(this ContainerBuilder builder, Assembly executingAssembly)
+        {
+            builder.RegisterAssemblyTypes(executingAssembly)
+                .Where(c => c.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces();
+
+            return builder;
+        }
+
+        public static void BuildContainer(this ContainerBuilder builder)
+        {
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+        }
+
+    }
+}
---
+++ src/MBran.Umbraco.Web/Handlers/AutofacEventHandler.cs
@@ -0,0 +1,42 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using System.Reflection;
+using Umbraco.Core;
+using Umbraco.Core.Services;
+using Umbraco.Web;
+
+namespace MBran.Umbraco.Web
+{
+    public class AutofacEventHandler : IApplicationEventHandler
+    {
+        public void OnApplicationInitialized(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            var builder = new ContainerBuilder();
+
+            builder.RegisterInstance(applicationContext.Services.ContentService).As<IContentService>();
+            builder.RegisterControllers(typeof(UmbracoApplication).Assembly);
+            builder.RegisterApiControllers(typeof(UmbracoApplication).Assembly);
+
+            builder.RegisterControllers(typeof(AutofacEventHandler).Assembly);
+
+            builder.RegisterUmbraco()
+                .RegisterCustomControllers(executingAssembly)
+                .RegisterServices(executingAssembly)
+                .RegisterRepositories(executingAssembly)
+                .RegisterModules()
+                .BuildContainer();
+        }
+
+        public void OnApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+
+        }
+
+        public void OnApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+
+        }
+    }
+}
