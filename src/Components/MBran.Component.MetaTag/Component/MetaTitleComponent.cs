21be35c23c7ad5e66c59b788c149c69b18aca6e9
Mark Gil Libres <markglibres@gmail.com>
2017-12-13 01:16:55 +1100

    added metatag component

---
+++ src/Components/MBran.Component.MetaTag/Component/MetaTitleComponent.cs
@@ -0,0 +1,23 @@
+﻿using MBran.Umbraco.Components;
+using System.Web.Mvc;
+
+namespace MBran.Component.Meta
+{
+    public class MetaTitleComponent : Component<MetaTagHeaderSurfaceController>
+    {
+        public MetaTitleComponent(HtmlHelper htmlHelper) 
+            : base(htmlHelper)
+        {
+
+        }
+
+        public override string RenderAction
+        {
+            get
+            {
+                return nameof(MetaTagHeaderSurfaceController.RenderTitle);
+            }
+        }
+
+    }
+}
---
+++ src/Components/MBran.Component.MetaTag/Controller/MetaTagHeaderSurfaceController.cs
@@ -0,0 +1,35 @@
+﻿using MBran.Core;
+using MBran.Core.Components;
+using System;
+using System.Web.Mvc;
+using Umbraco.Core.Models;
+
+namespace MBran.Component.Meta 
+{
+    public class MetaTagHeaderSurfaceController : ComponentSurfaceController
+    {
+        private readonly IMetaService _metaService;
+
+        public MetaTagHeaderSurfaceController(IPageHelper pageHelper,
+            IMetaService metaService) 
+            : base(pageHelper)
+        {
+            _metaService = metaService;
+        }
+        
+        [ChildActionOnly]
+        public PartialViewResult RenderTitle(IPublishedContent model = null)
+        {
+            MetaTitle dataModel = _metaService.GetHeader(model);
+            MetaTitleViewModel viewModel = dataModel.Map<MetaTitleViewModel>();
+
+            return ComponentView(viewModel);
+        }
+
+        [ChildActionOnly]
+        public override PartialViewResult RenderModel(IPublishedContent model = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
